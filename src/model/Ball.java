64e134ace9c977ac9f3988cc037a04093802545b
Cassio <cassio@dhcp206.merunorth110.iit.edu>
2015-06-22 11:00:38 -0500

    Adding basic classes

---
+++ src/model/Ball.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Pong ball.
+ * 
+ * @author Cassio dos Santos Sousa 
+ */
+public class Ball {
+	
+	// Ball's dimensions, position and speed
+	
+	public static final int radius = 3;
+	private double x, y;
+	private double speedX, speedY;
+	
+	// Constructor
+	
+	public Ball (){
+		x = Board.width / 2;
+		y = Board.height / 2;
+		speedX = 15.0;
+		speedY = 0.0;
+	}
+	
+}
---
+++ src/model/Board.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Pong board.
+ * 
+ * @author Cassio dos Santos Sousa
+ */
+public class Board {
+
+	// Board's dimensions
+	
+	public static final int width = 800;
+	public static final int height = 600;
+	
+}
