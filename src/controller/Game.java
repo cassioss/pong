c531d576a0bdf3291fe06137dd383475f416633e
Cassio <cassio@dhcp82.merunorth97.iit.edu>
2015-06-23 17:34:28 -0500

    Added basic View and Controller methods

---
+++ src/controller/Game.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javax.swing.JFrame;
+
+import view.Screen;
+
+public class Game {
+
+	private Screen ponGUI;
+	private JFrame pongFrame;
+
+	public Game() {
+
+	}
+
+	public void runGame() {
+
+	}
+
+}
---
+++ src/view/Screen.java
@@ -0,0 +1,12 @@
+package view;
+
+/**
+ * Pong screen.
+ * 
+ * @author Cassio dos Santos Sousa
+ */
+public class Screen {
+
+	
+	
+}
