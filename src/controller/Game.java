2c149e76e598a71c8c160d258bd2ee87064854af
Cassio <cassio@Cassio-MacBook-Pro.local>
2015-06-25 16:29:04 -0500

    Adjusted score board position

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -3,17 +3,25 @@ package controller;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
-import view.Screen;
+import view.Interface;
 
 public class Game {
+	
+	private static JFrame pongFrame;
+	
+	private static void setFrame(){
+		pongFrame = new JFrame("Pong");
+		pongFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		pongFrame.setSize(800, 600);
+		pongFrame.setLocation(250, 50);
+		pongFrame.setResizable(false);
+		pongFrame.add(new Interface());
+		pongFrame.setVisible(true);
+	}
 
-    public static void main(String[] args) {
-            JFrame frame = new JFrame("Hello");
-            frame.setPreferredSize(new Dimension(800, 600));
-            frame.add(new Screen());
-            frame.pack();
-            frame.setVisible(true);
-    }
+	public static void main(String[] args) {
+		setFrame();
+	}
 }
-
