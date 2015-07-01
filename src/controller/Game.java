d17bc2658df4541679b126ac927ca4483d742941
Cassio <cassio@dhcp82.merunorth97.iit.edu>
2015-06-30 22:59:09 -0500

    800x600 game positions according to screen size

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.awt.AWTEvent;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -11,13 +13,38 @@ import view.Interface;
 
 public class Game {
 
+	private static Dimension screenSize;
+	private static Interface ponGUI;
+	private static JFrame pongFrame;
+
 	protected static void createAndShowGui() {
-		Interface ponGUI = new Interface();
-		JFrame pongFrame = new JFrame("Pong");
+		getScreenSize();
+		ponGUI = new Interface();
+		createFrame();
+	}
+
+	private static void getScreenSize() {
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	}
+
+	private static int adjustX() {
+		double width = screenSize.getWidth();
+		int fullWidth = (int) width;
+		return (fullWidth - Board.WIDTH) / 2;
+	}
+
+	private static int adjustY() {
+		double height = screenSize.getHeight();
+		int fullHeight = (int) height;
+		return (fullHeight - Board.HEIGHT) / 2;
+	}
+
+	private static void createFrame() {
+		pongFrame = new JFrame("Pong");
 		pongFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		pongFrame.getContentPane().add(ponGUI);
 		pongFrame.setSize(Board.WIDTH, Board.HEIGHT);
-		pongFrame.setLocation(250, 50);
+		pongFrame.setLocation(adjustX(), adjustY());
 		pongFrame.setResizable(false);
 		pongFrame.setVisible(true);
 	}
